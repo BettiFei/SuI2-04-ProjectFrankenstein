2f2ab9aba7735219d45ffcbc45bf414ddedfa553
BettiFei <bettina.feibel@uni-bayreuth.de>
2025-12-12 20:56:36 +0100

    Partial bug fixes

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -20,17 +20,17 @@ const WALK_VELOCITY := 200.0
 const JUMP_VELOCITY := -600.0
 const JUMP_DECELERATION := 1500.0
 const DOUBLE_JUMP_VELOCITY := -450.0
-#const WALL_SLIDE_GRAVITY := 300.0
-#const WALL_SLIDE_VELOCITY := 500.0
+const WALL_SLIDE_GRAVITY := 300.0
+const WALL_SLIDE_VELOCITY := 500.0
 const DASH_LENGTH := 100.0
 const DASH_VELOCITY := 600.0
 
 @onready var anim_sprite: AnimatedSprite2D = %AnimatedSprite2D
 @onready var coyote_timer: Timer = $CoyoteTimer
 @onready var player_collider: CollisionShape2D = %PlayerCollider
-#@onready var ledge_climb_ray_cast: RayCast2D = %LedgeClimbRayCast
-#@onready var ledge_space_ray_cast: RayCast2D = %LedgeSpaceRayCast
-#@onready var wall_slide_ray_cast: RayCast2D = %WallSlideRayCast
+@onready var ledge_climb_ray_cast: RayCast2D = %LedgeClimbRayCast
+@onready var ledge_space_ray_cast: RayCast2D = %LedgeSpaceRayCast
+@onready var wall_slide_ray_cast: RayCast2D = %WallSlideRayCast
 @onready var dash_cooldown: Timer = $DashCooldown
 
 var active_state := STATE.FALL
@@ -42,7 +42,8 @@ var dash_jump_buffer := false
 
 func _ready() -> void:
 	switch_state(active_state) # properly start game with correct state
-	#ledge_climb_ray_cast.add_exception(self) # prevent raycast from detecting player's own coll. shape
+	ledge_climb_ray_cast.add_exception(self) # prevent raycast from detecting player's own coll. shape
+	#print("ledge_climb_offset: " + str(ledge_climb_offset()))
 
 func _physics_process(delta: float) -> void:
 	process_state(delta)
@@ -76,17 +77,17 @@ func switch_state(new_state: STATE) -> void:
 			velocity.y = DOUBLE_JUMP_VELOCITY
 			can_double_jump = false
 			
-		#STATE.LEDGE_CLIMB:
-			#anim_sprite.play("ledge_climb")
-			#velocity = Vector2.ZERO
-			#global_position.y = ledge_climb_ray_cast.get_collision_point().y # make player face direction of ledge & align position with ledge
-			#can_double_jump = true
+		STATE.LEDGE_CLIMB:
+			anim_sprite.play("ledge_climb")
+			velocity = Vector2.ZERO
+			global_position.y = ledge_climb_ray_cast.get_collision_point().y # make player face direction of ledge & align position with ledge
+			can_double_jump = true
 			
-		#STATE.WALL_SLIDE:
-			#anim_sprite.play("wall_slide")
-			#velocity.y = 0
-			#can_double_jump = true
-			#can_dash = true
+		STATE.WALL_SLIDE:
+			anim_sprite.play("wall_slide")
+			velocity.y = 0
+			can_double_jump = true
+			can_dash = true
 		
 		STATE.DASH:
 			if dash_cooldown.time_left > 0:
@@ -119,10 +120,10 @@ func process_state(delta: float) -> void:
 					switch_state(STATE.JUMP)
 				elif can_double_jump:
 					switch_state(STATE.DOUBLE_JUMP)
-			#elif is_input_toward_facing() and is_ledge() and is_space():
-				#switch_state(STATE.LEDGE_CLIMB)
-			#elif is_input_toward_facing() and can_wall_slide():
-				#switch_state(STATE.WALL_SLIDE)
+			elif is_input_toward_facing() and is_ledge() and is_space():
+				switch_state(STATE.LEDGE_CLIMB)
+			elif is_input_toward_facing() and can_wall_slide():
+				switch_state(STATE.WALL_SLIDE)
 			elif Input.is_action_just_pressed("dash") and can_dash:
 				switch_state(STATE.DASH)
 		
@@ -150,24 +151,24 @@ func process_state(delta: float) -> void:
 			elif Input.is_action_just_pressed("dash") and can_dash:
 				switch_state(STATE.DASH)
 		
-		#STATE.LEDGE_CLIMB:
-			#if not anim_sprite.is_playing():
-				#anim_sprite.play("idle")
-				#var offset := ledge_climb_offset()
-				#offset.x *= facing_direction
-				#position += offset
-				#switch_state(STATE.FLOOR)
+		STATE.LEDGE_CLIMB:
+			if not anim_sprite.is_playing():
+				anim_sprite.play("idle")
+				var offset := ledge_climb_offset()
+				offset.x *= facing_direction
+				position += offset
+				switch_state(STATE.FLOOR)
 				
-		#STATE.WALL_SLIDE:
-			#velocity.y = move_toward(velocity.y, WALL_SLIDE_VELOCITY, WALL_SLIDE_GRAVITY * delta)
-			#handle_movement()
-			#
-			#if is_on_floor():
-				#switch_state(STATE.FLOOR)
-			##elif is_ledge() and is_space():
-				##switch_state(STATE.LEDGE_CLIMB)
-			#elif not can_wall_slide():
-				#switch_state(STATE.FALL)
+		STATE.WALL_SLIDE:
+			velocity.y = move_toward(velocity.y, WALL_SLIDE_VELOCITY, WALL_SLIDE_GRAVITY * delta)
+			handle_movement()
+			
+			if is_on_floor():
+				switch_state(STATE.FLOOR)
+			elif is_ledge() and is_space():
+				switch_state(STATE.LEDGE_CLIMB)
+			elif not can_wall_slide():
+				switch_state(STATE.FALL)
 		
 		STATE.DASH:
 			dash_cooldown.start()
@@ -183,10 +184,10 @@ func process_state(delta: float) -> void:
 					switch_state(STATE.FLOOR)
 				else:
 					switch_state(STATE.FALL)
-			#elif is_ledge() and is_space():
-				#switch_state(STATE.LEDGE_CLIMB)
-			#elif can_wall_slide():
-				#switch_state(STATE.WALL_SLIDE)
+			elif is_ledge() and is_space():
+				switch_state(STATE.LEDGE_CLIMB)
+			elif can_wall_slide():
+				switch_state(STATE.WALL_SLIDE)
 
 func handle_movement(input_direction: float = 0) -> void:
 	if input_direction == 0:
@@ -198,36 +199,36 @@ func set_facing_direction(direction: float) -> void:
 	if direction:
 		anim_sprite.flip_h = direction < 0
 		facing_direction = direction
-		#ledge_climb_ray_cast.position.x = direction * absf(ledge_climb_ray_cast.position.x)
-		#ledge_climb_ray_cast.target_position.x = direction * absf(ledge_climb_ray_cast.position.x)
-		#ledge_climb_ray_cast.force_raycast_update()
-		#wall_slide_ray_cast.position.x = direction * absf(wall_slide_ray_cast.position.x)
-		#wall_slide_ray_cast.target_position.x = direction * absf(wall_slide_ray_cast.position.x)
-		#wall_slide_ray_cast.force_raycast_update()
+		ledge_climb_ray_cast.position.x = direction * absf(ledge_climb_ray_cast.position.x)
+		ledge_climb_ray_cast.target_position.x = direction * absf(ledge_climb_ray_cast.target_position.x)
+		ledge_climb_ray_cast.force_raycast_update()
+		wall_slide_ray_cast.position.x = direction * absf(wall_slide_ray_cast.position.x)
+		wall_slide_ray_cast.target_position.x = direction * absf(wall_slide_ray_cast.target_position.x)
+		wall_slide_ray_cast.force_raycast_update()
 
 # returns whether or not player is giving input towards a ledge:
 func is_input_toward_facing() -> bool:
 	return signf(Input.get_axis("move_left", "move_right")) == facing_direction
 
 # returns whether what we are colliding with is a ledge:
-#func is_ledge() -> bool: 
-	#return is_on_wall_only() and \
-	#ledge_climb_ray_cast.is_colliding() and \
-	#ledge_climb_ray_cast.get_collision_normal().is_equal_approx(Vector2.UP)
-	#
-## returns whether there is enough space for player on top of ledge:
-#func is_space() -> bool:
-	#ledge_space_ray_cast.global_position = ledge_climb_ray_cast.get_collision_point()
-	#ledge_space_ray_cast.force_raycast_update()
-	#return not ledge_space_ray_cast.is_colliding()
-#
-## perfectly snap player to top of the ledge:
-#func ledge_climb_offset() -> Vector2:
-	#var shape := player_collider.shape
-	#if shape is CapsuleShape2D:
-		#return Vector2(shape.radius * 2.0, -shape.height * 0.7)
-	#return Vector2.ZERO
-#
-## check if wall slide is possible:
-#func can_wall_slide() -> bool:
-	#return is_on_wall_only() and wall_slide_ray_cast.is_colliding()
+func is_ledge() -> bool: 
+	return is_on_wall_only() and \
+	ledge_climb_ray_cast.is_colliding() and \
+	ledge_climb_ray_cast.get_collision_normal().is_equal_approx(Vector2.UP)
+	
+# returns whether there is enough space for player on top of ledge:
+func is_space() -> bool:
+	ledge_space_ray_cast.global_position = ledge_climb_ray_cast.get_collision_point()
+	ledge_space_ray_cast.force_raycast_update()
+	return not ledge_space_ray_cast.is_colliding()
+
+# perfectly snap player to top of the ledge:
+func ledge_climb_offset() -> Vector2:
+	var shape := player_collider.shape
+	if shape is CapsuleShape2D:
+		return Vector2(shape.radius * 2.0, -shape.height * 0.5)
+	return Vector2.ZERO
+
+# check if wall slide is possible:
+func can_wall_slide() -> bool:
+	return is_on_wall_only() and wall_slide_ray_cast.is_colliding()
